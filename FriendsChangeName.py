84502246baf67b9478952b153cda5590240a4992
manoj8412 <manoj8412@gmail.com>
2017-08-21 22:02:43 -0400

    Update FriendsChangeName.py

--- FriendsChangeName.py
+++ FriendsChangeName.py
@@ -1,14 +1,14 @@
 import os
-def CreateR():
-	with open("/Users/manojkumar/Documents/F3.txt") as f:
-		R = ['.DS_Store']
+def CreateR(): # This will create a list of names of episodes that I use as source.
+	with open("path.txt") as f:
+		R = ['.DS_Store'] # some times the folder has this file so its name gets changed.
 		for x in f:
 			R.append(x.split('\n')[0])
 
 #Friends.S01E01 - The One where Monica gets a Roommate - 720p.BluRay.x264PAHE.in.mkv
 
 def rename_files():
-	path  = "/Volumes/Macintosh HD/Friends/Friends S10 720p BluRay x264 PaHe.in"
+	path  = "path/Friends S10 720p BluRay x264 PaHe.in"
 	file_list = os.listdir(path)
 	#print file_list
 	#saved_path = os.getcwd()
